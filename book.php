8bf68cfb6d3c58b4abd11819eea2c3e6fc006c9b
Carlos <Carlos.tuuling@ametikool.ee>
2021-10-19 14:14:14 +0300

    yes based

--- book.php
+++ book.php
@@ -1,3 +1,10 @@
 <?php
+require_once 'connection.php';
+$stmt = $pdo ->prepare ('SELECT * FROM authors WHERE first_name=:first_name');
 
+$stmt-> execute([':first_name' => $_GET['name']]);
+$author = $stmt->fetch();
+var_dump($author);
+echo "<br><br>";
+echo $author['last_name'];
 echo $_GET["id"];
