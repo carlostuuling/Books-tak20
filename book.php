0614f7a8aca3bfa8a30665be1be4791998b52a8d
Carlos <Carlos.tuuling@ametikool.ee>
2021-10-25 14:20:06 +0300

    bababo

--- book.php
+++ book.php
@@ -2,14 +2,28 @@
 
 require_once 'connection.php';
 
-$stmt = $pdo ->prepare ('SELECT * FROM books WHERE id=:id');
+$bookId = $_GET['id'];
+
+$stmt = $pdo ->prepare ('SELECT * FROM books b LEFT JOIN book_authors ba ON b.id=ba.book_id LEFT JOIN authors a ON a.id=ba.author_id WHERE b.id=:id');
 
 $stmt-> execute([':id' => $_GET['id']]);
 $book = $stmt->fetch();
 ?>
-<html>
-<p>Raamatu nimi: <?php echo $book['title']; ?></p>
-<p>Väljalaske aasta: <?php echo $book['release_date']; ?></p>
-<p>keel: <?php echo $book['language']; ?></p>
-<p>autor: <?php echo $book['']; ?></p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Raamatu nimi: <?php echo $book['title']; ?></p>
+    <p>Väljalaske aasta: <?php echo $book['release_date']; ?></p>
+    <p>Autor: <?php echo $book['first_name']; ?> <?php echo $book['last_name']; ?></p>
+    <p>hind: <?php echo $book['price']; ?></p>
+    <p> <a href="edit.php?id=<?=$bookId?>">muuda</a></p>
+    <p> <a href="delete.php??id=<?=$bookId?>">kustuta</a></p>
+
+</body>
 </html>
