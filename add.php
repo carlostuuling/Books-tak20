cc2102bd66c0a65ad1036af2611d79a02e4fd7b3
Carlos <Carlos.tuuling@ametikool.ee>
2021-11-01 11:45:37 +0200

    woofwoof meow meow

--- add.php
+++ add.php
@@ -2,12 +2,10 @@
     require_once "connection.php";
 
     if ( isset($_POST['save'])) {
-        var_dump( $_POST);
-   // $stmt = $pdo->prepare('UPDATE books SET title=:title WHERE id=:id');
+        $stmt = $pdo->prepare('INSERT INTO authors(first_name, last_name) VALUES (:first_name, :last_name);');
+        $stmt->execute(['first_name' => $_POST["first_name"] , 'last_name' => $_POST["last_name"]]);
     }
-   // $stmt = $pdo->prepare('INSERT INTO books WHERE id=:id');
-   // $stmt->execute([':id' => $_POST["id"]]);
-   // header('Location: index.php');
+    $stmt = $pdo->query('SELECT * FROM authors order by id desc');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -19,9 +17,17 @@
 </head>
 <body>
 <form action="add.php" method="post">
-        <input type="text" name="first-name">
-        <input type="text" name="last-name">
+        <input type="text" name="first_name">
+        <input type="text" name="last_name">
         <input type="submit" name="save" value="salvesta">
     </form>
+    <h1>sinu ema isa</h1>
+    <ul>
+        <?php while( $authors = $stmt->fetch()): ?>
+        <li><?=$authors['first_name'];?>
+            <?=$authors['last_name'];?>
+        </li>
+        <?php endwhile; ?>
+    </ul>
 </body>
 </html>
